3bbeb892d746836068f6e702a9f4a8304ffc6e94
tomeekk20 <th.szewczyk@t-online.de>
2019-04-02 15:54:54 +0200

    Updated mobile navigation

--- themes/flex-landing/js/main.js
+++ themes/flex-landing/js/main.js
@@ -3,11 +3,16 @@
 const mobileNav = document.querySelector('.mobile-icon');
 const navItems = document.querySelector('.mobile-nav');
 const closeBtn = document.querySelector('.close');
+const mobileItem = document.querySelector('.mobile-item');
 
 mobileNav.addEventListener('click', function(e){
   navItems.classList.add('active-toggle');
 })
 
 closeBtn.addEventListener('click', function(e){
+  navItems.classList.remove('active-toggle');
+})
+
+mobileItem.addEventListener('click', function(e){
   navItems.classList.remove('active-toggle');
 })
