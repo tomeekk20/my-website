18b5481d7cfc230b633f83714bb6a9befb88226c
tomeekk20 <th.szewczyk@t-online.de>
2019-04-15 14:04:26 +0200

    Updated CSS with BEM Methodology

--- themes/flex-landing/js/main.js
+++ themes/flex-landing/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
-
+/*
 const mobileNav = document.querySelector('.mobile-icon');
-const navItems = document.querySelector('.mobile-nav');
+const mainNav = document.querySelector('.mobile-nav');
 const closeBtn = document.querySelector('.close');
 const mobileItem = document.querySelector('.mobile-item');
 
@@ -15,4 +15,12 @@ closeBtn.addEventListener('click', function(e){
 
 mobileItem.addEventListener('click', function(e){
   navItems.classList.remove('active-toggle');
-});
+}); */
+
+function classToggle() {
+  const nav = document.querySelector('.mobile-nav');
+
+  nav.classList.toggle('active-toggle');
+}
+const mobileItems = document.querySelectorAll('.mobile-item');
+  mobileItems.forEach(item => item.addEventListener('click', classToggle));
